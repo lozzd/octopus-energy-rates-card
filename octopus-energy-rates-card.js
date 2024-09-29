237203006e242e830e37c035a9a3a5e380ffc37d
Gareth Cooper <gcoop1979@gmail.com>
2024-09-29 09:03:48 +0100

    Working but ugly TargetTimes Editor

--- octopus-energy-rates-card.js
+++ octopus-energy-rates-card.js
@@ -263,6 +263,28 @@ class OctopusEnergyRatesCardEditor extends LitElement {
 
   setConfig(config) {
     this._config = { ...OctopusEnergyRatesCard.getDefaultConfig(), ...config };
+    this._config.targetTimes = this._config.targetTimes || [];
+  }
+
+  static get styles() {
+    return css`
+      .targetTimes {
+        border: 1px solid var(--divider-color);
+        padding: 10px;
+        margin-top: 10px;
+      }
+      .targetTime {
+        display: flex;
+        align-items: center;
+        margin-bottom: 10px;
+      }
+      .targetTime > * {
+        margin-right: 10px;
+      }
+      .targetTime ha-icon-button {
+        --mdc-icon-button-size: 24px;
+      }
+    `;
   }
 
   render() {
@@ -375,14 +397,80 @@ class OctopusEnergyRatesCardEditor extends LitElement {
         .computeLabel=${(schema) => schema.label || schema.name}
         @value-changed=${this._valueChanged}
       ></ha-form>
+      <div class="targetTimes">
+        <h3>Target Times</h3>
+        ${this._config.targetTimes.map((targetTime, index) =>
+          this._renderTargetTime(targetTime, index)
+        )}
+        <ha-icon-button
+          .path=${"M12,20C7.59,20 4,16.41 4,12C4,7.59 7.59,4 12,4C16.41,4 20,7.59 20,12C20,16.41 16.41,20 12,20M12,2A10,10 0 0,0 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M13,7H11V11H7V13H11V17H13V13H17V11H13V7Z"}
+          @click=${this._addTargetTime}
+        ></ha-icon-button>
+      </div>
     `;
   }
 
-  _valueChanged(ev) {
-    const config = ev.detail.value;
-    this._config = config;
+  _renderTargetTime(targetTime, index) {
+    return html`
+      <div class="targetTime">
+        <ha-entity-picker
+          .hass=${this.hass}
+          .value=${targetTime.entity}
+          .includeDomains=${["binary_sensor"]}
+          @change=${(e) =>
+            this._updateTargetTime(index, "entity", e.target.value)}
+        ></ha-entity-picker>
+        <ha-textfield
+          .value=${targetTime.backgroundColor}
+          label="Background Color"
+          @change=${(e) =>
+            this._updateTargetTime(index, "backgroundColor", e.target.value)}
+        ></ha-textfield>
+        <ha-icon-picker
+          .value=${targetTime.prefix}
+          @value-changed=${(e) =>
+            this._updateTargetTime(index, "prefix", e.detail.value)}
+        ></ha-icon-picker>
+        <ha-icon-button
+          .path=${"M19,6.41L17.59,5L12,10.59L6.41,5L5,6.41L10.59,12L5,17.59L6.41,19L12,13.41L17.59,19L19,17.59L13.41,12L19,6.41Z"}
+          @click=${() => this._removeTargetTime(index)}
+        ></ha-icon-button>
+      </div>
+    `;
+  }
+
+  _addTargetTime() {
+    this._config = {
+      ...this._config,
+      targetTimes: [
+        ...this._config.targetTimes,
+        { entity: "", backgroundColor: "", prefix: "" },
+      ],
+    };
+    this._valueChanged();
+  }
+
+  _removeTargetTime(index) {
+    this._config = {
+      ...this._config,
+      targetTimes: this._config.targetTimes.filter((_, i) => i !== index),
+    };
+    this._valueChanged();
+  }
+
+  _updateTargetTime(index, key, value) {
+    this._config = {
+      ...this._config,
+      targetTimes: this._config.targetTimes.map((item, i) =>
+        i === index ? { ...item, [key]: value } : item
+      ),
+    };
+    this._valueChanged();
+  }
+
+  _valueChanged() {
     this.dispatchEvent(
-      new CustomEvent("config-changed", { detail: { config } })
+      new CustomEvent("config-changed", { detail: { config: this._config } })
     );
   }
 }
@@ -397,6 +485,6 @@ window.customCards = window.customCards || [];
 window.customCards.push({
   type: "octopus-energy-rates-card",
   name: "Octopus Energy Rates Card",
-  preview: false,
-  description: "This card displays the energy rates for Octopus Energy",
+  preview: true,
+  description: "Displays the energy rates for Octopus Energy",
 });
